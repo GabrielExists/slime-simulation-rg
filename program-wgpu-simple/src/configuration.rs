3f774c7bca202ee83aa385d4f8e081366c5a038e
GabrielExists <gabriel.exists.concurrently@gmail.com>
2025-02-27 23:14:32 +0100

    Diffusy pattern

--- program-wgpu-simple/src/configuration.rs
+++ program-wgpu-simple/src/configuration.rs
@@ -8,7 +8,7 @@ pub const AGENT_STATS: [AgentStats; 1] = [AgentStats {
 // Percent of full white to black transition per second.
 // 100.0 is completely faded after 1 second.
 // 50.0 is completely faded after 2 seconds.
-pub const EVAPORATION_SPEED: f32 = 5.0;
+pub const EVAPORATION_SPEED: f32 = 30.0;
 
 // Speed of diffusion in percent.
 // Reaching 90% takes 1 second if set to 240%.
