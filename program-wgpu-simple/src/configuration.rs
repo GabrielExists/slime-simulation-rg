1e9f3b9b62249361e77268e25fb68a319a3850a1
GabrielExists <gabriel.exists.concurrently@gmail.com>
2025-02-28 15:24:18 +0100

    Marmor

--- program-wgpu-simple/src/configuration.rs
+++ program-wgpu-simple/src/configuration.rs
@@ -14,16 +14,17 @@ pub enum SpawnMode {
     },
 
 }
-pub const SPAWN_MODE: SpawnMode = SpawnMode::CircleFacingInwards {
-    max_distance: 200.0,
-};
+// pub const SPAWN_MODE: SpawnMode = SpawnMode::CircleFacingInwards {
+//     max_distance: 200.0,
+// };
+pub const SPAWN_MODE: SpawnMode = SpawnMode::EvenlyDistributed;
 pub const TIME_SCALE: f32 = 1.0;
 pub const COMPUTE_STEPS_PER_RENDER: u32 = 1;
 pub const NUM_AGENTS: u32 = 10000;
 pub const AGENT_STATS: [AgentStats; 1] = [AgentStats {
     // Pixels travelled per second
-    velocity: 80.0,
-    turn_speed: PI * 8.0,
+    velocity: 50.0,
+    turn_speed: PI * 10.0,
     sensor_angle_spacing: PI / 3.0,
     sensor_offset: 5.0,
     pixel_addition: 0x0FFFFFFF,
@@ -31,10 +32,10 @@ pub const AGENT_STATS: [AgentStats; 1] = [AgentStats {
 // Percent of full white to black transition per second.
 // 100.0 is completely faded after 1 second.
 // 50.0 is completely faded after 2 seconds.
-pub const EVAPORATION_SPEED: f32 = 100.0;
+pub const EVAPORATION_SPEED: f32 = 80.0;
 
 // Speed of diffusion in percent.
 // Reaching 90% takes 1 second if set to 240%.
 // Reaching 86% takes 1 second if set to 200%.
 // Reaching 63% takes 1 second if set to 100%.
-pub const DIFFUSION_SPEED: f32 = 130.0;
+pub const DIFFUSION_SPEED: f32 = 180.0;
