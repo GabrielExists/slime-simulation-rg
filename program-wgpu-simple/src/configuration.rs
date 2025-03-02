59ee3bef0f9c5b9097f8c68e26b4be29d43c29ef
Gabriel <gabriel.exists.concurrently@gmail.com>
2025-03-02 19:10:18 +0100

    It was a massacre

--- program-wgpu-simple/src/configuration.rs
+++ program-wgpu-simple/src/configuration.rs
@@ -15,38 +15,38 @@ pub const COMPUTE_STEPS_PER_RENDER: u32 = 1;
 pub const AGENT_STATS: [AgentStatsAll; NUM_AGENT_TYPES] = [
     // Blue - Xenophilic
     AgentStatsAll {
-        spawn_mode: SpawnMode::CircumferenceFacingOutward { distance: 170.0 },
-        num_agents: 10000,
+        spawn_mode: SpawnMode::CircleFacingInwards { max_distance: 100.0 },
+        num_agents: 1_000_000,
         shader_stats: AgentStats {
             velocity: 65.0,
             turn_speed: PI * 80.0,
             turn_speed_avoidance: PI * 30.0,
             sensor_angle_spacing: PI / 3.0,
             sensor_offset: 5.0,
-            pixel_addition: 1.0 / 5.0,
+            pixel_addition: 1.0 / 100.0,
             avoidance_threshold: 20.0,
             evaporation_speed: 50.0,
             diffusion_speed: 180.0,
-            attraction_blue: 0.0,
+            attraction_blue: 0.2,
             attraction_green: 1.0,
         },
     },
     // Green
     AgentStatsAll {
-        spawn_mode: SpawnMode::CircumferenceFacingClockwise { distance: 170.0 },
-        num_agents: 10000,
+        spawn_mode: SpawnMode::CircumferenceFacingOutward { distance: 170.0 },
+        num_agents: 1_000_000,
         shader_stats: AgentStats {
             velocity: 65.0,
             turn_speed: PI * 80.0,
             turn_speed_avoidance: PI * 30.0,
             sensor_angle_spacing: PI / 3.0,
             sensor_offset: 5.0,
-            pixel_addition: 1.0 / 5.0,
+            pixel_addition: 1.0 / 100.0,
             avoidance_threshold: 10.0,
             evaporation_speed: 50.0,
             diffusion_speed: 180.0,
             attraction_blue: -1.0,
-            attraction_green: 1.0,
+            attraction_green: 0.6,
         },
     },
 ];
